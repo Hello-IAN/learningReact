46b9a54941a41f01c13fef66905c8715be6be1c6
IAN <ian@IANui-MacBookAir.local>
2023-01-09 21:39:19 +0900

    lala

---
+++ 01tuto/src/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+	<header>
+		<h1>
+			For test Header
+		</h1>
+	</header>
+
+  )
+}
+
+export default Header
---
+++ 04tuto/src/App.js
@@ -0,0 +1,15 @@
+import Header from './Header.js';
+import Content from './Content.js';
+import Footer from './Footer.js';
+import './index.css'
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
